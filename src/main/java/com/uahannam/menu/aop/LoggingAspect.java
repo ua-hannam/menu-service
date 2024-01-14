32f163100b76754edb1bb56d082dcacea1d30119
pgrrr <pgrrr119@gmail.com>
2024-01-14 14:17:50 +0900

    feat: Logging AOP 수정 및 에러 코드 추가
    - 중복 코드 제거
    - 카테고리 에러 코드 추가

--- src/main/java/com/uahannam/menu/aop/LoggingAspect.java
+++ src/main/java/com/uahannam/menu/aop/LoggingAspect.java
@@ -5,21 +5,41 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 @Slf4j
 @Aspect
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* com.uahannam.menu.*.*(..))")
+    @Pointcut("execution(* com.aop.controller..*.*(..))")
+    private void cut(){}
+
+    @Before("cut()")
     public void logBefore(JoinPoint joinPoint) {
-        log.info("Before calling method: {}", joinPoint.getSignature().getName());
+        Method method = getMethod(joinPoint);
+        log.info("Before calling method: {}", method.getName());
+
+        Object[] args = joinPoint.getArgs();
+        if (args.length == 0) log.info("no parameter");
+        for (Object arg : args) {
+            log.info("parameter type = {}", arg.getClass().getSimpleName());
+            log.info("parameter value = {}", arg);
+        }
     }
 
     @After("execution(* com.uahannam.menu.*.*(..))")
     public void logMethodEnd(JoinPoint joinPoint) {
         System.out.println("Method End: " + joinPoint.getSignature().getName());
+    }
+
+    private Method getMethod(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        return signature.getMethod();
     }
 }
 
